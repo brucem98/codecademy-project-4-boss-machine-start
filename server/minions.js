193ae26f9ed1cd7a3c8ea134f2a0bc37522dae0e
Bruce Milliken <84877032+brucem98@users.noreply.github.com>
2023-11-10 12:46:37 +0000

    Setup work routers in minions.js

--- server/minions.js
+++ server/minions.js
@@ -47,3 +47,39 @@ minionsRouter.delete("/:minionId", (req, res, next) => {
   }
   res.send();
 });
+
+minionsRouter.get("/:minionId/work", (req, res, next) => {
+  const work = getAllFromDatabase("work").filter((singleWork) => {
+    return singleWork.minionId === req.params.minionId;
+  });
+  res.send(work);
+});
+
+minionsRouter.post("/:minionId/work", (req, res, next) => {
+  const workToAdd = req.body;
+  workToAdd.minionId = req.params.minionId;
+  // It adds the minionId from the route parameters to the workToAdd object wjicj we get from the request body.
+  // This ensures that the new work being added is associated with the correct minion.
+  const newWork = addToDatabase("work", workToAdd);
+  res.status(201).send(newWork);
+});
+
+minionsRouter.put("/:minionId/work/:workId", (req, res, next) => {
+  if (req.params.minionId !== req.body.minionId) {
+    res.status(400).send();
+  } else {
+    updatedWork = updateInstanceInDatabase("work", req.body);
+    res.send(updatedWork);
+  }
+});
+
+// route handler is designed to delete a specific work item associated with a particular minion
+minionsRouter.delete("/:miniondId/work/:workId", (req, res, next) => {
+  const deleted = deleteFromDatabasebyId("work", req.params.workId);
+  if (deleted) {
+    res.status(204);
+  } else {
+    res.status(500);
+  }
+  res.send();
+});
